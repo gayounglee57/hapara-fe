f858998b208a9711682a93ffe2be2877a144e60c
Gayoung Lee <gayounglee57@gmail.com>
2025-03-13 09:37:32 +1300

    Added classnames to format better

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -4,6 +4,7 @@ import { useEffect, useState } from "react";
 import { Tile } from "../components/Tile";
 import { StudentsResponse } from "../types";
 import axios from "axios";
+import cn from "classnames";
 
 export default function Dashboard() {
   const [students, setStudents] = useState<StudentsResponse>([]);
@@ -25,7 +26,16 @@ export default function Dashboard() {
   if (loading) return <p>Loading...</p>;
 
   return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
+    <div
+      className={cn(
+        "grid min-h-screen",
+        "grid-rows-[20px_1fr_20px]",
+        "items-center justify-items-center",
+        "gap-16",
+        "p-8 pb-20 sm:p-20",
+        "font-[family-name:var(--font-geist-sans)]"
+      )}
+    >
       <div>Dashboard</div>
       <div className="grid grid-cols-3 grid-rows-3 gap-[32px] row-start-2 sm:items-start">
         {students.map((student) => (
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,18 @@
 import Image from "next/image";
+import cn from "classnames";
 
 export default function Home() {
   return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
+    <div
+      className={cn(
+        "grid min-h-screen",
+        "grid-rows-[20px_1fr_20px]",
+        "items-center justify-items-center",
+        "gap-16",
+        "p-8 pb-20 sm:p-20",
+        "font-[family-name:var(--font-geist-sans)]"
+      )}
+    >
       <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start">
         <Image
           className="dark:invert"
@@ -15,7 +25,18 @@ export default function Home() {
 
         <div className="flex gap-4 items-center flex-col sm:flex-row">
           <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:w-auto"
+            className={cn(
+              "rounded-full border border-solid border-transparent",
+              "transition-colors",
+              "flex items-center justify-center gap-2",
+              "bg-foreground text-background",
+              "hover:bg-[#383838] dark:hover:bg-[#ccc]",
+              "font-medium",
+              "text-sm sm:text-base",
+              "h-10 sm:h-12",
+              "px-4 sm:px-5",
+              "sm:w-auto"
+            )}
             href="/dashboard"
             rel="noopener noreferrer"
           >
